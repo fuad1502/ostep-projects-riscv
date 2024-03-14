720570a09725aa29344dee10b44906dc6a59d0e9
Fuad Ismail <fuad1502@gmail.com>
2024-03-14 13:50:01 +0700

    Modified initial-xv6 to use environment variables to find the xv6 directory

--- initial-xv6/test-getreadcount.sh
+++ initial-xv6/test-getreadcount.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-
-if ! [[ -d src ]]; then
+if ! [[ -d $XV6_DIR ]]; then
     echo "The src/ dir does not exist."
     echo "Your xv6 code should be in the src/ directory"
     echo "to enable the automatic tester to work."
